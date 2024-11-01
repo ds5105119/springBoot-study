4f84fb2df8c38237b8504645234e6d9c172790b2
IIH <ds5105119@gmail.com>
2024-11-02 03:20:27 +0900

    add View and Class

---
+++ src/main/java/IIH/demo/controller/HelloController.java
@@ -0,0 +1,43 @@
+package IIH.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!!");
+        return "hello";
+    }
+
+    @GetMapping("hellomvc")
+    public String helloMvc(@RequestParam(value="name", required = false) String name, Model model) {
+        model.addAttribute("data", name != null ? name : "아무것도 없음");
+        return "hello-mvc";
+    }
+
+    @GetMapping("helloapi")
+    @ResponseBody
+    public Hello helloApi(@RequestParam(value="name", required = false) String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
